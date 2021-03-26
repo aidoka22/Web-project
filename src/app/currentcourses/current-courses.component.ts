46fdf3bd4ea1f165455ec84c7ad7db64b0347f39
aidoka22 <65388424+aidoka22@users.noreply.github.com>
2021-03-26 20:07:14 +0600

    Add files via upload

---
+++ src/app/currentcourses/current-courses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-courses',
+  templateUrl: './current-courses.component.html',
+  styleUrls: ['./current-courses.component.css']
+})
+export class CurrentCoursesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
