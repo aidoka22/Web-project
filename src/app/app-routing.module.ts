ea14a29bab64fcd9c354958cd9888c9ebceae29d
aidoka22 <65388424+aidoka22@users.noreply.github.com>
2021-03-26 21:16:22 +0600

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent} from './login-page/login-page.component';
+import { CreateUserComponent} from './create-user/create-user.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { CurrentCoursesComponent } from './current-courses/current-courses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomePageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'create', component: CreateUserComponent},
+  {path:'user-info',component: UserInfoComponent},
+  {path:'wishlist',component:WishlistComponent},
+  {path:'current-courses',component:CurrentCoursesComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseDetailsComponent } from './course-details/course-details.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { CurrentCoursesComponent } from './current-courses/current-courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseDetailsComponent,
+    CourseListComponent,
+    CreateUserComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    UserInfoComponent,
+    WishlistComponent,
+    CurrentCoursesComponent
   ],
   imports: [
     BrowserModule,
