f9e7d8b74f7e9d68978ddcb65b613f13e638da4c
aidoka22 <65388424+aidoka22@users.noreply.github.com>
2021-04-20 19:24:33 +0600

    Add files via upload

--- src/app/wishlist/wishlist.component.ts
+++ src/app/wishlist/wishlist.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {WishListService} from "../wishlist.service";
 
 @Component({
   selector: 'app-wishlist',
   templateUrl: './wishlist.component.html',
   styleUrls: ['./wishlist.component.css']
 })
-export class WishlistComponent implements OnInit {
+export class WishListComponent implements OnInit {
+  items = this.wishlistService.getItems();
 
-  constructor() { }
+  constructor(private wishlistService: WishListService) { }
 
   ngOnInit(): void {
   }
 
+  clearCart() {
+    this.items = this.wishlistService.clearWishlist();
+  }
+
 }
+
