65b38b6f0eed82b632f16eb7eb4f40074764f454
aidoka22 <65388424+aidoka22@users.noreply.github.com>
2021-04-20 20:08:23 +0600

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,18 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent} from './login-page/login-page.component';
 import { CreateUserComponent} from './create-user/create-user.component';
 import { UserInfoComponent } from './user-info/user-info.component';
-import { WishlistComponent } from './wishlist/wishlist.component';
+import { WishListComponent } from './wishlist/wishlist.component';
 import { CurrentCoursesComponent } from './current-courses/current-courses.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   {path: 'home', component: HomePageComponent},
   {path: 'login', component: LoginPageComponent},
   {path: 'create', component: CreateUserComponent},
   {path:'user-info',component: UserInfoComponent},
-  {path:'wishlist',component:WishlistComponent},
+  {path:'wishlist',component:WishListComponent},
   {path:'current-courses',component:CurrentCoursesComponent},
-  {path: 'courses', component: CourseListComponent},
-  {path: 'courses/:id', component: CourseDetailsComponent},
-  {path: 'cart', component: CartComponent},
+  {path:'cart',component:CartComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
