322d84f63adb048b11b42caafa754255c1aeb7c4
aidoka22 <65388424+aidoka22@users.noreply.github.com>
2021-04-24 00:01:32 +0600

    Add files via upload

--- src/app/wishlist/wishlist.component.ts
+++ src/app/wishlist/wishlist.component.ts
@@ -19,4 +19,11 @@ export class WishlistComponent implements OnInit {
   clearWishList(){
     this.wishs=this.wishListService.clearWishList();
   }
+  deleteFromCart(wish){
+    if(this.wishs.length==1){
+      this.clearWishList();
+    }
+    else{
+    this.wishs=this.wishs.filter(obj => obj !==wish);
+  }}
 }
