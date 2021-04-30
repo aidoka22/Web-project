c913c981ee919f0523d7f049831651f8892d4a58
aidoka22 <65388424+aidoka22@users.noreply.github.com>
2021-05-01 03:31:04 +0600

    Add files via upload

---
+++ django/academy/api/view/__init__.py
@@ -0,0 +1,6 @@
+from .views_course_fbv import course_list, course_detail,teacher_courses
+from .views_course_cbv import CourseListAPIView, CourseDetailAPIView
+from .views_category_cbv import CategoryListAPIView, CategoryDetailAPIView
+from .view_wishlist_fbv import wishlist, list_wishlist,clear_wishlist
+from .views_cart_fbv import cart, list_cart,clear_cart
+from .views_current_courses_fbv import current_course, list_current_courses, buy_current_courses
---
+++ django/academy/api/view/views_course_fbv.py
@@ -0,0 +1,43 @@
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
+from api.models import Course
+from api.serializers import CourseSerializer2
+
+@api_view(['GET', 'POST'])
+def course_list(request):
+    if request.method == 'GET':
+        courses = Course.objects.all()
+        serializer = CourseSerializer2(courses, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = CourseSerializer2(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+@api_view(['GET'])
+def teacher_courses(request,teacher_id):
+    if request.method == 'GET':
+        courses = Course.objects.get(author=teacher_id)
+        serializer = CourseSerializer2(courses, many=True)
+        return Response(serializer.data)
+@api_view(['GET', 'PUT', 'DELETE'])
+def course_detail(request, course_id):
+    try:
+        course = Course.objects.get(id=course_id)
+    except Course.DoesNotExist as e:
+        return Response({'message': str(e)}, status=400)
+
+    if request.method == 'GET':
+        serializer = CourseSerializer2(course)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = CourseSerializer2(instance=course, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    elif request.method == 'DELETE':
+        course.delete()
+        return Response({'message': 'deleted'}, status=204)
