3074299df533eb236cb9a1bed21a5abe0630717d
shagyrovmaksat <60479259+shagyrovmaksat@users.noreply.github.com>
2021-04-30 14:15:49 +0500

    Add files via upload

---
+++ src/app/create-user/create-user.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {RegisterService} from "../register.service";
+
+@Component({
+  selector: 'app-create-user',
+  templateUrl: './create-user.component.html',
+  styleUrls: ['./create-user.component.css']
+})
+export class CreateUserComponent implements OnInit {
+
+  name = '';
+  surname = '';
+  username = '';
+  password = '';
+  category = '';
+  teacher = false;
+
+  constructor(private registerService: RegisterService) { }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    this.registerService.register(this.username, '', this.password);
+    console.log(1);
+  }
+}
---
+++ src/app/login-page/login-page.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {LoginService} from '../login.service';
+import {AppComponent} from '../app.component';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+
+export class LoginPageComponent implements OnInit{
+
+  username = '';
+  password = '';
+  teacher = false;
+
+  constructor(private loginService: LoginService, private appComponent: AppComponent) { }
+
+  ngOnInit(): void {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.appComponent.logged = true;
+    }
+  }
+
+  login() {
+    // console.log(this.username)
+    this.loginService.login(this.username, this.password).subscribe( (data) => {
+      localStorage.setItem('token', data.token);
+      this.appComponent.logged = true;
+      this.username = '';
+      this.password = '';
+    });
+    // this.appComponent.logged = true;
+    // this.username = '';
+    // this.password = '';
+  }
+}
