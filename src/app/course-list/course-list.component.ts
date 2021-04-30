1b277e99cf3e83f54588f8a6f24312fcd1ace8d7
aliyakm <57257098+aliyakm@users.noreply.github.com>
2021-04-30 18:33:45 +0600

    Add files via upload

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {Course} from "../models";
-import {CoursesService} from "../courses.service";
-
+import {Course} from '../models';
+import {Category} from '../models';
+import {CoursesService} from '../courses.service';
+import {CategoryService} from '../category.service';
+import {ActivatedRoute} from '@angular/router';
 
 
 @Component({
@@ -10,24 +12,46 @@ import {CoursesService} from "../courses.service";
   styleUrls: ['./course-list.component.css']
 })
 export class CourseListComponent implements OnInit {
-  courses: Course[];
+  courses: Course[] = [];
+  categories: Category[] = [];
+  category: Category;
 
-  constructor(private coursesService: CoursesService) {
+  constructor(private route: ActivatedRoute, private coursesService: CoursesService, private categoryService: CategoryService) {
     // this.courses = [];
   }
 
   ngOnInit(): void {
-    this.getCourses();
+    // this.getCourses();
+    this.getCategories();
     // this.courses = COURSES;
+    // this.route.paramMap.subscribe((params) => {
+    //   const id = +params.get('id');
+    //   this.getCategory(id);
+    // });
   }
 
   getCourses() {
-    this.courses = this.coursesService.getCourses();
+    this.coursesService.getCourses().subscribe((data) => {
+      this.courses = data;
+    });
   }
 
   scroll($element): void {
     console.log($element);
     $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  }
+
+  getCategories() {
+    this.categoryService.getCategories().subscribe((data) => {
+      this.categories = data;
+    });
+  }
+
+  getCategory(id: number) {
+    this.categoryService.getCategory(id).subscribe((data) => {
+      // this.category = data;
+      this.courses = data;
+    });
   }
 
   // scroll(el: HTMLElement) {
