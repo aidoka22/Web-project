53a0de0f7d8903812519d31931c27dc47aec99ba
aidoka22 <65388424+aidoka22@users.noreply.github.com>
2021-05-01 03:25:24 +0600

    Add files via upload

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,21 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import {CartService} from '../cart.service';
-
+import { Course } from '../models';
+import {WishListService} from '../wishlist.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  items = this.cartService.getItems();
+  items :Course[]=[];
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService,
+    private wishListService:WishListService,) { }
 
   ngOnInit(): void {
+    this.getCart();
   }
-
+  getCart(){
+    this.cartService.getItems().subscribe((data) => {
+      this.items = data;
+    });
+  }
+  addToWishList(item){
+    this.wishListService.addToList(item);
+    this.deleteFromCart(item);
+   }
   clearCart() {
-    this.items = this.cartService.clearCart();
+    return this.cartService.clearCart().subscribe((data) => {
+      this.items = data;
+    });
   }
-
+  
+  deleteFromCart(item){
+    this.items=this.items.filter(obj => obj !==item);
+    return this.cartService.deletefromCart(item).subscribe((data) => {
+      item = data;
+    });
+  }
 }
