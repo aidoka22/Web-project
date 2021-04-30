f5babc4d9e16ede6ed72828e44f82bbf801d27ce
aidoka22 <65388424+aidoka22@users.noreply.github.com>
2021-05-01 03:24:35 +0600

    Add files via upload

--- src/app/wishlist/wishlist.component.ts
+++ src/app/wishlist/wishlist.component.ts
@@ -1,29 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import {WishListService} from '../wishlist.service';
 import {CartService} from '../cart.service';
+import { Course } from '../models';
 @Component({
   selector: 'app-wishlist',
   templateUrl: './wishlist.component.html',
   styleUrls: ['./wishlist.component.css']
 })
 export class WishlistComponent implements OnInit {
-  wishs=this.wishListService.getwishs();
+  wishs:Course[]=[];
   constructor(private wishListService:WishListService,
     private cartService:CartService) { }
 
   ngOnInit(): void {
+    this.getWishList();
+  }
+  getWishList(){
+    this.wishListService.getwishs().subscribe((data) => {
+      this.wishs = data;
+    });
   }
   addToCart(wish){
       this.cartService.addToCart(wish);
+      this.deleteFromWishList(wish);
   }
   clearWishList(){
-    this.wishs=this.wishListService.clearWishList();
+    return this.wishListService.clearWishList().subscribe((data) => {
+      this.wishs = data;
+    });
   }
-  deleteFromCart(wish){
-    if(this.wishs.length==1){
-      this.clearWishList();
-    }
-    else{
+  deleteFromWishList(wish){ 
     this.wishs=this.wishs.filter(obj => obj !==wish);
-  }}
+    return this.wishListService.deletefromWishlist(wish).subscribe((data) => {
+      wish = data;
+    });
+   
+  }
 }
