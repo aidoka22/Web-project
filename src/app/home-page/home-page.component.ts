7b98467ab9c08ee98f683fc72829365a559bd981
aliyakm <57257098+aliyakm@users.noreply.github.com>
2021-04-30 18:36:20 +0600

    Add files via upload

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AppComponent} from '../app.component';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  logged = false;
+  student = false;
+
+  constructor(private appComponent: AppComponent) { }
 
   ngOnInit(): void {
+    this.logged = this.appComponent.logged;
+    this.student = this.appComponent.student;
   }
-
 }
