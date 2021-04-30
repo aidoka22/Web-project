b492f01cc60664f30085fdb23553126ccae29a74
shagyrovmaksat <60479259+shagyrovmaksat@users.noreply.github.com>
2021-04-30 17:56:18 +0500

    Delete django directory

--- django/academy/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from api.models import Teacher, Student, Category, Course
-
-admin.site.register(Teacher)
-admin.site.register(Student)
-admin.site.register(Category)
-admin.site.register(Course)
