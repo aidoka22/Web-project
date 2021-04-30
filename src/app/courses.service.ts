02eda1cd1030bf2d6d1e72c713856adbdb2ee170
aliyakm <57257098+aliyakm@users.noreply.github.com>
2021-04-30 16:04:58 +0600

    Update courses.service.ts

--- src/app/courses.service.ts
+++ src/app/courses.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
-import {COURSES} from "./courses-db";
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Course} from './models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor() { }
+  BASE_URL = 'http://localhost:8000';
 
-  getCourses() {
-    return COURSES;
-  }
-  getCourse(id: number) {
-    return COURSES.find((x) => x.id === id);
+  constructor(private http: HttpClient) { }
+
+  // addToCourse(course){
+  //   COURSES.push(course)
+  // }
+
+  getCourses(): Observable<Course[]>{
+    return this.http.get<Course[]>(`${this.BASE_URL}/api/categories/courses/`);
   }
 
+  getCourse(id: number): Observable<Course> {
+    return this.http.get<Course>(`${this.BASE_URL}/api/categories/courses/${id}`);
+  }
 }
